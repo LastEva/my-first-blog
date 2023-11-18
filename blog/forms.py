0a1a89021d3ec95b0b4d4483d39300fb7614c02e
LastEva <azarnin.ilya@mail.ru>
2023-11-18 10:28:25 +0300

    Обновление дизайна

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,8 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
-
+    title = forms.CharField(label='Заголовок', widget=forms.Textarea())
+    text = forms.CharField(label='Содержание', widget=forms.Textarea())
     class Meta:
         model = Post
         fields = ('title', 'text',)
